2ec16bea0d1474020865b51094be392c89b2070d
Selwyn Kramer <selwyn@darker.industries>
2022-05-26 12:21:30 +0200

    Bump the windows crate to 0.37.0

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    #[cfg(target_family = "windows")]
-    windows::build! {
-        Windows::Win32::{
-            Networking::WinSock::{SOCKADDR_IN,SOCKADDR_IN6},
-            NetworkManagement::IpHelper::{ConvertLengthToIpv4Mask, GetAdaptersAddresses},
-        }
-    };
-}
