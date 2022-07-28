fea514182a15a16a7de16a63eafee56e57c6ba1c
Esteban Borai <estebanborai@gmail.com>
2022-07-27 21:39:51 -0400

    fix: scope c libs build for macos

--- build.rs
+++ build.rs
@@ -1,12 +1,14 @@
-#[cfg(target_os = "macos")]
 fn main() {
-    use cc::Build;
-    use std::path::Path;
+    #[cfg(target_os = "macos")]
+    {
+        use cc::Build;
+        use std::path::Path;
 
-    let path = Path::new("src")
-        .join("target")
-        .join("macos")
-        .join("helpers.c");
+        let path = Path::new("src")
+            .join("target")
+            .join("macos")
+            .join("helpers.c");
 
-    Build::new().file(path).compile("helpers");
+        Build::new().file(path).compile("helpers");
+    }
 }
