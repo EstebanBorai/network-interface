39ac7dc3b697582f61f463c1999e923468e28fa9
Esteban Borai <estebanborai@gmail.com>
2022-08-05 20:54:09 -0400

    feat(linux): support for `AF_PACKET` netifas

--- src/target/linux.rs
+++ src/target/linux.rs
@@ -1,12 +1,26 @@
+use std::collections::HashMap;
 use std::mem;
 use std::net::{Ipv4Addr, Ipv6Addr};
 use std::slice::from_raw_parts;
 
-use libc::{getifaddrs, ifaddrs, sockaddr_in, sockaddr_in6, strlen, AF_INET, AF_INET6, malloc};
+use libc::{
+    getifaddrs, ifaddrs, sockaddr_in, sockaddr_in6, strlen, AF_PACKET, AF_INET, AF_INET6, malloc,
+    sockaddr_ll,
+};
 
 use crate::interface::Netmask;
 use crate::{Error, NetworkInterface, NetworkInterfaceConfig, Result};
 use crate::utils::{ipv4_from_in_addr, ipv6_from_in6_addr, make_ipv4_netmask, make_ipv6_netmask};
+
+/// 16. Length of the string form for IP.
+///
+/// Reference: https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/netinet_in.h.html
+pub const INET_ADDRSTRLEN: usize = 16;
+
+/// 46. Length of the string form for IPv6.
+///
+/// Reference: https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/netinet_in.h.html
+pub const INET6_ADDRSTRLEN: usize = 46;
 
 /// `ifaddrs` struct raw pointer alias
 pub type NetIfaAddrPtr = *mut *mut ifaddrs;
@@ -26,7 +40,7 @@ impl NetworkInterfaceConfig for NetworkInterface {
             ));
         }
 
-        let mut network_interfaces: Vec<NetworkInterface> = Vec::new();
+        let mut network_interfaces: HashMap<String, NetworkInterface> = HashMap::new();
         let netifa = addr;
 
         let has_current = |netifa: NetIfaAddrPtr| {
@@ -39,23 +53,33 @@ impl NetworkInterfaceConfig for NetworkInterface {
 
         let mut advance = |network_interface: Option<NetworkInterface>| {
             if let Some(network_interface) = network_interface {
-                network_interfaces.push(network_interface);
+                network_interfaces.insert(network_interface.name.clone(), network_interface);
             }
 
             unsafe { *netifa = (**netifa).ifa_next };
         };
 
+        let mut mac_addresses: HashMap<String, String> = HashMap::new();
+
         while has_current(netifa) {
             let netifa_addr = unsafe { (**netifa).ifa_addr };
 
-            let netifa_family = if netifa_addr.is_null() {
-                None
-            } else {
-                Some(unsafe { (*netifa_addr).sa_family as i32 })
-            };
+            if netifa_addr.is_null() {
+                advance(None);
+            }
+
+            let netifa_family = unsafe { (*netifa_addr).sa_family as i32 };
 
             match netifa_family {
-                Some(AF_INET) => {
+                AF_PACKET => {
+                    let name = make_netifa_name(&netifa)?;
+                    let mac = make_mac_addrs(&netifa)?;
+
+                    mac_addresses.insert(name, mac);
+
+                    advance(None)
+                }
+                AF_INET => {
                     let netifa_addr = netifa_addr;
                     let socket_addr = netifa_addr as *mut sockaddr_in;
                     let internet_address = unsafe { (*socket_addr).sin_addr };
@@ -68,7 +92,7 @@ impl NetworkInterfaceConfig for NetworkInterface {
 
                     advance(Some(network_interface));
                 }
-                Some(AF_INET6) => {
+                AF_INET6 => {
                     let netifa_addr = netifa_addr;
                     let socket_addr = netifa_addr as *mut sockaddr_in6;
                     let internet_address = unsafe { (*socket_addr).sin6_addr };
@@ -87,7 +111,13 @@ impl NetworkInterfaceConfig for NetworkInterface {
             }
         }
 
-        Ok(network_interfaces)
+        for (netifa_name, mac_addr) in mac_addresses {
+            if let Some(netifa) = network_interfaces.get_mut(&netifa_name) {
+                netifa.mac_addr = Some(mac_addr);
+            }
+        }
+
+        Ok(network_interfaces.into_values().collect())
     }
 }
 
@@ -144,4 +174,16 @@ fn make_ipv6_broadcast_addr(netifa: &NetIfaAddrPtr) -> Result<Option<Ipv6Addr>> 
     let addr = ipv6_from_in6_addr(&internet_address)?;
 
     Ok(Some(addr))
+}
+
+fn make_mac_addrs(netifa: &NetIfaAddrPtr) -> Result<String> {
+    let netifa_addr = netifa;
+    let name = make_netifa_name(&netifa)?;
+    let saddr_ll: *mut *mut sockaddr_ll =
+        unsafe { mem::transmute::<&*mut *mut ifaddrs, *mut *mut sockaddr_ll>(netifa_addr) };
+    let ifa_addr = unsafe { (*(*saddr_ll)).sll_addr };
+
+    println!("{}: {:?}", name, ifa_addr);
+
+    Ok(String::default())
 }
