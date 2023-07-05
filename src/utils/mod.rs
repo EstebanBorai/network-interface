db454a7ae25c8b516097879261e507400788be31
Sebastian Urban <surban@surban.net>
2023-07-05 17:09:58 +0200

    Add Android support
    Android supports access to network interfaces via standard Linux socket
    API via the Android NDK. Thus we treat Android the same way as Linux.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,7 @@
 #[cfg(windows)]
 pub mod hex;
-#[cfg(any(target_os = "linux", target_os = "macos"))]
+#[cfg(any(target_os = "android", target_os = "linux", target_os = "macos"))]
 mod unix;
 
-#[cfg(any(target_os = "linux", target_os = "macos"))]
+#[cfg(any(target_os = "android", target_os = "linux", target_os = "macos"))]
 pub use unix::*;
